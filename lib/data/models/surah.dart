c77bfc44ef7583db049b7bcdcedb226d76b7f6f4
Berdi Gumantara <berdi.g.interns@aux.gojek.com>
2022-08-15 15:32:08 +0700

    try to get surah

---
+++ lib/data/models/surah.dart
@@ -0,0 +1,52 @@
+// API : https://equran.id/api/surat
+// Get all Surah in Al-Qur'an
+
+import 'dart:convert';
+
+Surah surahFromJson(String str) => Surah.fromJson(json.decode(str));
+
+String surahToJson(Surah data) => json.encode(data.toJson());
+
+class Surah {
+  Surah({
+    this.nomor,
+    this.nama,
+    this.namaLatin,
+    this.jumlahAyat,
+    this.tempatTurun,
+    this.arti,
+    this.deskripsi,
+    this.audio,
+  });
+
+  int? nomor;
+  String? nama;
+  String? namaLatin;
+  int? jumlahAyat;
+  String? tempatTurun;
+  String? arti;
+  String? deskripsi;
+  String? audio;
+
+  factory Surah.fromJson(Map<String, dynamic> json) => Surah(
+        nomor: json["nomor"],
+        nama: json["nama"],
+        namaLatin: json["nama_latin"],
+        jumlahAyat: json["jumlah_ayat"],
+        tempatTurun: json["tempat_turun"],
+        arti: json["arti"],
+        deskripsi: json["deskripsi"],
+        audio: json["audio"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "nomor": nomor,
+        "nama": nama,
+        "nama_latin": namaLatin,
+        "jumlah_ayat": jumlahAyat,
+        "tempat_turun": tempatTurun,
+        "arti": arti,
+        "deskripsi": deskripsi,
+        "audio": audio,
+      };
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,12 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+void main() async {
+  Uri url = Uri.parse("https://equran.id/api/surat");
 
-import 'package:alquran/main.dart';
+  var res = await http.get(url);
+  List data = json.decode(res.body);
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  // 1-114 -> index of 1113 = an-nas
+  print(data[113]);
 }
