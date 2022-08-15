4b8229e43ee8ba79ba1f14fbc954b2e740bcc3b5
Berdi Gumantara <berdi.g.interns@aux.gojek.com>
2022-08-15 19:06:32 +0700

    change model and api

--- lib/data/models/detail_surah.dart
+++ lib/data/models/detail_surah.dart
@@ -1,4 +1,4 @@
-// API : https://equran.id/api/surat/${no_surat}
+// API : https://api.quran.gading.dev/surah/${no_surat}
 // Get all Surah in Al-Qur'an
 
 import 'dart:convert';
@@ -10,147 +10,365 @@ String detailSurahToJson(DetailSurah data) => json.encode(data.toJson());
 
 class DetailSurah {
   DetailSurah({
-    this.status,
-    this.nomor,
-    this.nama,
-    this.namaLatin,
-    this.jumlahAyat,
-    this.tempatTurun,
-    this.arti,
-    this.deskripsi,
-    this.audio,
-    this.ayat,
-    this.suratSelanjutnya,
-    this.suratSebelumnya,
+    this.number,
+    this.sequence,
+    this.numberOfVerses,
+    this.name,
+    this.revelation,
+    this.tafsir,
+    this.preBismillah,
+    this.verses,
   });
 
-  bool? status;
-  int? nomor;
-  String? nama;
-  String? namaLatin;
-  int? jumlahAyat;
-  String? tempatTurun;
-  String? arti;
-  String? deskripsi;
-  String? audio;
-  List<Ayat>? ayat;
-  bool? suratSelanjutnya;
-  SuratSebelumnya? suratSebelumnya;
+  int? number;
+  int? sequence;
+  int? numberOfVerses;
+  Name? name;
+  Revelation? revelation;
+  DataTafsir? tafsir;
+  PreBismillah? preBismillah;
+  List<Verse>? verses;
 
   factory DetailSurah.fromJson(Map<String, dynamic> json) => DetailSurah(
-        status: json["status"],
-        nomor: json["nomor"],
-        nama: json["nama"],
-        namaLatin: json["nama_latin"],
-        jumlahAyat: json["jumlah_ayat"],
-        tempatTurun: json["tempat_turun"],
-        arti: json["arti"],
-        deskripsi: json["deskripsi"],
-        audio: json["audio"],
-        ayat: List<Ayat>.from(json["ayat"].map((x) => Ayat.fromJson(x))),
-        suratSelanjutnya: json["surat_selanjutnya"],
-        suratSebelumnya: SuratSebelumnya.fromJson(json["surat_sebelumnya"]),
+        number: json["number"],
+        sequence: json["sequence"],
+        numberOfVerses: json["numberOfVerses"],
+        name: Name.fromJson(json["name"]),
+        revelation: Revelation.fromJson(json["revelation"]),
+        tafsir: DataTafsir.fromJson(json["tafsir"]),
+        preBismillah: PreBismillah.fromJson(json["preBismillah"]),
+        verses: List<Verse>.from(json["verses"].map((x) => Verse.fromJson(x))),
       );
 
   Map<String, dynamic> toJson() => {
-        "status": status,
-        "nomor": nomor,
-        "nama": nama,
-        "nama_latin": namaLatin,
-        "jumlah_ayat": jumlahAyat,
-        "tempat_turun": tempatTurun,
-        "arti": arti,
-        "deskripsi": deskripsi,
-        "audio": audio,
-        "ayat": ayat == null
+        "number": number,
+        "sequence": sequence,
+        "numberOfVerses": numberOfVerses,
+        "name": name?.toJson(),
+        "revelation": revelation?.toJson(),
+        "tafsir": tafsir?.toJson(),
+        "preBismillah": preBismillah?.toJson(),
+        "verses": verses == null
             ? null
-            : List<dynamic>.from(ayat!.map((x) => x.toJson())),
-        "surat_selanjutnya": suratSelanjutnya,
-        "surat_sebelumnya": suratSebelumnya?.toJson(),
+            : List<dynamic>.from(verses!.map((x) => x.toJson())),
       };
 }
 
-class Ayat {
-  Ayat({
-    this.id,
-    this.surah,
-    this.nomor,
-    this.ar,
-    this.tr,
-    this.idn,
+class Name {
+  Name({
+    this.short,
+    this.long,
+    this.transliteration,
+    this.translation,
   });
 
-  int? id;
-  int? surah;
-  int? nomor;
-  String? ar;
-  String? tr;
-  String? idn;
+  String? short;
+  String? long;
+  Translation? transliteration;
+  Translation? translation;
 
-  factory Ayat.fromJson(Map<String, dynamic> json) => Ayat(
-        id: json["id"],
-        surah: json["surah"],
-        nomor: json["nomor"],
-        ar: json["ar"],
-        tr: json["tr"],
-        idn: json["idn"],
+  factory Name.fromJson(Map<String, dynamic> json) => Name(
+        short: json["short"],
+        long: json["long"],
+        transliteration: Translation.fromJson(json["transliteration"]),
+        translation: Translation.fromJson(json["translation"]),
       );
 
   Map<String, dynamic> toJson() => {
-        "id": id,
-        "surah": surah,
-        "nomor": nomor,
-        "ar": ar,
-        "tr": tr,
-        "idn": idn,
+        "short": short,
+        "long": long,
+        "transliteration": transliteration?.toJson(),
+        "translation": translation?.toJson(),
       };
 }
 
-class SuratSebelumnya {
-  SuratSebelumnya({
+class Translation {
+  Translation({
+    this.en,
     this.id,
-    this.nomor,
-    this.nama,
-    this.namaLatin,
-    this.jumlahAyat,
-    this.tempatTurun,
-    this.arti,
-    this.deskripsi,
+  });
+
+  String? en;
+  String? id;
+
+  factory Translation.fromJson(Map<String, dynamic> json) => Translation(
+        en: json["en"],
+        id: json["id"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "en": en,
+        "id": id,
+      };
+}
+
+class PreBismillah {
+  PreBismillah({
+    this.text,
+    this.translation,
     this.audio,
   });
 
-  int? id;
-  int? nomor;
-  String? nama;
-  String? namaLatin;
-  int? jumlahAyat;
-  String? tempatTurun;
-  String? arti;
-  String? deskripsi;
-  String? audio;
+  Text? text;
+  Translation? translation;
+  Audio? audio;
 
-  factory SuratSebelumnya.fromJson(Map<String, dynamic> json) =>
-      SuratSebelumnya(
+  factory PreBismillah.fromJson(Map<String, dynamic> json) => PreBismillah(
+        text: Text.fromJson(json["text"]),
+        translation: Translation.fromJson(json["translation"]),
+        audio: Audio.fromJson(json["audio"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "text": text?.toJson(),
+        "translation": translation?.toJson(),
+        "audio": audio?.toJson(),
+      };
+}
+
+class Audio {
+  Audio({
+    this.primary,
+    this.secondary,
+  });
+
+  String? primary;
+  List<String>? secondary;
+
+  factory Audio.fromJson(Map<String, dynamic> json) => Audio(
+        primary: json["primary"],
+        secondary: List<String>.from(json["secondary"].map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "primary": primary,
+        "secondary": secondary == null
+            ? null
+            : List<dynamic>.from(secondary!.map((x) => x)),
+      };
+}
+
+class Text {
+  Text({
+    this.arab,
+    this.transliteration,
+  });
+
+  String? arab;
+  Transliteration? transliteration;
+
+  factory Text.fromJson(Map<String, dynamic> json) => Text(
+        arab: json["arab"],
+        transliteration: Transliteration.fromJson(json["transliteration"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "arab": arab,
+        "transliteration": transliteration?.toJson(),
+      };
+}
+
+class Transliteration {
+  Transliteration({
+    this.en,
+  });
+
+  String? en;
+
+  factory Transliteration.fromJson(Map<String, dynamic> json) =>
+      Transliteration(
+        en: json["en"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "en": en,
+      };
+}
+
+class Revelation {
+  Revelation({
+    this.arab,
+    this.en,
+    this.id,
+  });
+
+  String? arab;
+  String? en;
+  String? id;
+
+  factory Revelation.fromJson(Map<String, dynamic> json) => Revelation(
+        arab: json["arab"],
+        en: json["en"],
         id: json["id"],
-        nomor: json["nomor"],
-        nama: json["nama"],
-        namaLatin: json["nama_latin"],
-        jumlahAyat: json["jumlah_ayat"],
-        tempatTurun: json["tempat_turun"],
-        arti: json["arti"],
-        deskripsi: json["deskripsi"],
-        audio: json["audio"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "arab": arab,
+        "en": en,
+        "id": id,
+      };
+}
+
+class DataTafsir {
+  DataTafsir({
+    this.id,
+  });
+
+  String? id;
+
+  factory DataTafsir.fromJson(Map<String, dynamic> json) => DataTafsir(
+        id: json["id"],
       );
 
   Map<String, dynamic> toJson() => {
         "id": id,
-        "nomor": nomor,
-        "nama": nama,
-        "nama_latin": namaLatin,
-        "jumlah_ayat": jumlahAyat,
-        "tempat_turun": tempatTurun,
-        "arti": arti,
-        "deskripsi": deskripsi,
-        "audio": audio,
+      };
+}
+
+class Verse {
+  Verse({
+    this.number,
+    this.meta,
+    this.text,
+    this.translation,
+    this.audio,
+    this.tafsir,
+  });
+
+  Number? number;
+  Meta? meta;
+  Text? text;
+  Translation? translation;
+  Audio? audio;
+  VerseTafsir? tafsir;
+
+  factory Verse.fromJson(Map<String, dynamic> json) => Verse(
+        number: Number.fromJson(json["number"]),
+        meta: Meta.fromJson(json["meta"]),
+        text: Text.fromJson(json["text"]),
+        translation: Translation.fromJson(json["translation"]),
+        audio: Audio.fromJson(json["audio"]),
+        tafsir: VerseTafsir.fromJson(json["tafsir"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "number": number?.toJson(),
+        "meta": meta?.toJson(),
+        "text": text?.toJson(),
+        "translation": translation?.toJson(),
+        "audio": audio?.toJson(),
+        "tafsir": tafsir?.toJson(),
+      };
+}
+
+class Meta {
+  Meta({
+    this.juz,
+    this.page,
+    this.manzil,
+    this.ruku,
+    this.hizbQuarter,
+    this.sajda,
+  });
+
+  int? juz;
+  int? page;
+  int? manzil;
+  int? ruku;
+  int? hizbQuarter;
+  Sajda? sajda;
+
+  factory Meta.fromJson(Map<String, dynamic> json) => Meta(
+        juz: json["juz"],
+        page: json["page"],
+        manzil: json["manzil"],
+        ruku: json["ruku"],
+        hizbQuarter: json["hizbQuarter"],
+        sajda: Sajda.fromJson(json["sajda"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "juz": juz,
+        "page": page,
+        "manzil": manzil,
+        "ruku": ruku,
+        "hizbQuarter": hizbQuarter,
+        "sajda": sajda?.toJson(),
+      };
+}
+
+class Sajda {
+  Sajda({
+    this.recommended,
+    this.obligatory,
+  });
+
+  bool? recommended;
+  bool? obligatory;
+
+  factory Sajda.fromJson(Map<String, dynamic> json) => Sajda(
+        recommended: json["recommended"],
+        obligatory: json["obligatory"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "recommended": recommended,
+        "obligatory": obligatory,
+      };
+}
+
+class Number {
+  Number({
+    this.inQuran,
+    this.inSurah,
+  });
+
+  int? inQuran;
+  int? inSurah;
+
+  factory Number.fromJson(Map<String, dynamic> json) => Number(
+        inQuran: json["inQuran"],
+        inSurah: json["inSurah"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "inQuran": inQuran,
+        "inSurah": inSurah,
+      };
+}
+
+class VerseTafsir {
+  VerseTafsir({
+    this.id,
+  });
+
+  Id? id;
+
+  factory VerseTafsir.fromJson(Map<String, dynamic> json) => VerseTafsir(
+        id: Id.fromJson(json["id"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id?.toJson(),
+      };
+}
+
+class Id {
+  Id({
+    this.short,
+    this.long,
+  });
+
+  String? short;
+  String? long;
+
+  factory Id.fromJson(Map<String, dynamic> json) => Id(
+        short: json["short"],
+        long: json["long"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "short": short,
+        "long": long,
       };
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,10 +4,10 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() async {
-  Uri url = Uri.parse("https://equran.id/api/surat");
+  Uri url = Uri.parse("https://api.quran.gading.dev/surah");
 
   var res = await http.get(url);
-  List data = json.decode(res.body);
+  List data = (json.decode(res.body) as Map<String, dynamic>)["data"];
 
   // 1-114 -> index of 1113 = an-nas
   // print(data[113]);
@@ -15,27 +15,23 @@ void main() async {
   // changes raw data api to Models Surah
   Surah surahAnnas = Surah.fromJson(data[113]);
 
-  print(surahAnnas.nomor);
-  print("=========================");
-  print(surahAnnas.nama);
-  print("=========================");
-  print(surahAnnas.namaLatin);
-  print("=========================");
-  print(surahAnnas.jumlahAyat);
-  print("=========================");
-  print(surahAnnas.deskripsi);
-  print("=========================");
-  print(surahAnnas.audio);
-  print("=========================");
+  print(surahAnnas.name);
+  print(surahAnnas.number);
+  print(surahAnnas.numberOfVerses);
+  print(surahAnnas.revelation);
+  print(surahAnnas.sequence);
+  print(surahAnnas.tafsir);
 
-  Uri urlAnnas = Uri.parse("https://equran.id/api/surat/${surahAnnas.nomor}");
+  Uri urlAnnas =
+      Uri.parse("https://api.quran.gading.dev/surah/${surahAnnas.number}");
 
   var resAnnas = await http.get(urlAnnas);
-  List dataAnnas = (json.decode(resAnnas.body) as Map<String, dynamic>)["ayat"];
+  Map<String, dynamic> dataAnnas =
+      (json.decode(resAnnas.body) as Map<String, dynamic>)["data"];
 
-  print(dataAnnas[5]);
+  // print(dataAnnas[5]);
 
   // changes raw data api to Models Surah
-  // DetailSurah ayatAnnas = DetailSurah.fromJson(dataAnnas[5]);
-  // print(ayatAnnas);
+  DetailSurah annas = DetailSurah.fromJson(dataAnnas);
+  print(annas.verses![5].text!.arab);
 }
